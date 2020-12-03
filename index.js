e98927e444e507a5dd73451afaa55c419a5e59dd
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-03 13:36:34 -0800

    Added dynamic port for future heroku deployment.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const server = require('./server');
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n* Server Running on http://localhost:${port} *\n`);
 })
